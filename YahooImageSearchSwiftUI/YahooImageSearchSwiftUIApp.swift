8de09bdbee7bc1f6973227b95f8d48cfad1df919
katafuchix <katafuchix@gmail.com>
2021-05-09 15:41:29 +0900

    Initial Commit

---
+++ YahooImageSearchSwiftUI/YahooImageSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YahooImageSearchSwiftUIApp.swift
+//  YahooImageSearchSwiftUI
+//
+//  Created by cano on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct YahooImageSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
